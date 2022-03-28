1d8905304a162e1d44fcb91336b994c89c24c3e1
Paweł Łęczycki <pawel@msd.info.pl>
2022-03-28 11:54:32 +0200

    Budowa programu

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,9 +1,18 @@
-﻿using System;
+﻿//USING - deklarujemy, że będziemy korzystać z klasy wskazanej przestrzeni nazw
+using System;
 
+// PRZESTRZEŃ NAZW - swego rodzaju adres, pod którym "mieszkają" klasy
 namespace ConsoleApp
 {
+    //KLASA - opisuje zachowania i właściwości obiektów (instancje klas), które będą na jej potstawie wytwarzane
+    //Pełna nazwa klasy to namespace + nazwa klasy
+    //Modyfikatory dostępu: public (dostępna wszędzie) i internal (tylko w obrębie assembly)
     class Program
     {
+        //METODA - typ zwracany + nazwa + nawiasy okrągłe + opcjonalne parametry + ciało w nawiasach klamrowych
+        //Modyfikatory dostępu: public, internal, private (dostępna tylko w obrebie klasy), protected (dostępna w obrębie klasy i klas dziedziczących)
+        //brak wskazania modyfikatora dostępu = private
+        //STATIC - jest to metoda niezależna od instancji klasy (może być wywołana bez potrzeby tworzenia obiektu danej klasy)
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
