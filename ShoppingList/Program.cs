61efa8060675edf5eaa23f4e532b714754107e17
Paweł Łęczycki <pawel@msd.info.pl>
2022-03-29 12:01:03 +0200

    Lista

--- ShoppingList/Program.cs
+++ ShoppingList/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ShoppingList.Models;
 using System;
+using System.Collections.Generic;
 
 namespace ShoppingList
 {
@@ -8,12 +9,30 @@ namespace ShoppingList
         static void Main(string[] args)
         {
             //wykorzystanie inicjalizatora przy tworzeniu obiektu - może to zastąpić liczne konstruktory
-            var item1 = new Item() {Name = "mleko"};
+            var item1 = new Item() {Name = "kawa"};
             var item2 = new Item() { Name = "mleko", Quantity = 1 };
             var item3 = new Item() { Quantity = 3 };
             var item4 = new Item();
 
             Console.WriteLine(item1.ToString());
+
+            List<Item> items = new List<Item>();
+            items.Add(item1);
+            items.Add(item2);
+            items.Add(item3);
+            items.Add(item4);
+
+            //indeksowanie od 0, dlatego podajemy indeks 1 aby wyciągnąć element 2
+            Console.WriteLine(items[1]);
+
+
+            //for (int i = 0; i < items.Count; i++)
+            //{
+            //    var item = items[i];
+            foreach(var item in items)
+            { 
+                Console.WriteLine(item);
+            }
         }
     }
 }
