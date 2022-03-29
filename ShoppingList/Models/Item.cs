6d521502bb83712cf1c92d39bc770dae629280d1
Paweł Łęczycki <pawel@msd.info.pl>
2022-03-29 11:48:25 +0200

    Tworzenie własnej klasy

---
+++ ShoppingList/Models/Item.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Models
+{
+    public class Item
+    {
+
+        /*
+//pole
+private string name;
+//metody dostępowe do pola (getter i setter)
+public void SetName(string value)
+{
+name = value;
+//if(name == null)
+//    name = value.ToUpper();
+}
+public string GetName()
+{
+return name;
+}
+*/
+        
+
+        private int quantity; // back-field dla property
+        //Właściwość - property
+
+        //auto-property
+        public string Name { get; set; }
+
+        //full-property
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                if(value > 0)
+                    quantity = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} : {Quantity}";
+        }
+    }
+}
---
+++ ShoppingList/Program.cs
@@ -0,0 +1,19 @@
+﻿using ShoppingList.Models;
+using System;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //wykorzystanie inicjalizatora przy tworzeniu obiektu - może to zastąpić liczne konstruktory
+            var item1 = new Item() {Name = "mleko"};
+            var item2 = new Item() { Name = "mleko", Quantity = 1 };
+            var item3 = new Item() { Quantity = 3 };
+            var item4 = new Item();
+
+            Console.WriteLine(item1.ToString());
+        }
+    }
+}
